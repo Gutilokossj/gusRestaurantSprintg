b141a43b02245c351184d0c59bfceb1f25c97108
Gutilokossj <gutilokossj12@gmail.com>
2024-08-13 12:29:00 -0300

    Adicionando ClientService

--- src/main/java/br/com/gusrestaurantspring/services/ClientService.java
+++ src/main/java/br/com/gusrestaurantspring/services/ClientService.java
@@ -6,6 +6,8 @@ import br.com.gusrestaurantspring.models.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,6 +22,43 @@ public class ClientService {
     public Client create(ClientDto clientDto) {
         Client client = new Client();
         client.setId(UUID.randomUUID());
+        client.setName(clientDto.name());
+        client.setDocument(clientDto.document());
+        return clientRepository.save(client);
+    }
 
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    public Client findById(UUID id) {
+        return clientRepository.findById(id).orElseThrow(() -> new RuntimeException("ID not found"));
+    }
+
+    public List<Client> findByName(String name) {
+        return clientRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public List<Client> findByDocument(String document) {
+        return clientRepository.findByDocumentContainingIgnoreCase(document);
+    }
+
+    public Client updateClient(UUID id, ClientDto clientDto) {
+        Optional<Client> optionalClient = clientRepository.findById(id);
+        Client client = null;
+        if(optionalClient.isPresent()) {
+            client = optionalClient.get();
+            client.setName(clientDto.name());
+            client.setDocument(clientDto.document());
+        } else {
+            throw new RuntimeException("Client not found");
+        }
+
+        return clientRepository.save(client);
+    }
+
+    public void deleteClient(UUID id) {
+        Client client = clientRepository.findById(id).orElseThrow(() -> new RuntimeException("ID not found"));
+        clientRepository.delete(client);
     }
 }
