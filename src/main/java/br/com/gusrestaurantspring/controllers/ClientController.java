cd82066d96b6bd54119d2f32a74f87106ef7cc6e
Gutilokossj <gutilokossj12@gmail.com>
2024-08-15 17:55:19 -0300

    Finalizando ClientController

--- src/main/java/br/com/gusrestaurantspring/controllers/ClientController.java
+++ src/main/java/br/com/gusrestaurantspring/controllers/ClientController.java
@@ -4,9 +4,11 @@ import br.com.gusrestaurantspring.models.dtos.ClientDto;
 import br.com.gusrestaurantspring.models.entities.Client;
 import br.com.gusrestaurantspring.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/client")
@@ -15,7 +17,40 @@ public class ClientController {
     @Autowired
     ClientService clientService;
 
+    @PostMapping
     public Client create(@RequestBody ClientDto clientDto) {
         return clientService.create(clientDto);
     }
+
+    @GetMapping
+    public List<Client> findAll(){
+        return clientService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Client findById(@PathVariable UUID id) {
+        return clientService.findById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public List<Client> findByName(@PathVariable String name) {
+        return clientService.findByName(name);
+    }
+
+    @GetMapping("/document/{document}")
+    public List<Client> findByDocument(@PathVariable String document) {
+        return clientService.findByDocument(document);
+    }
+
+    @PutMapping("/{id}")
+    public Client update(@PathVariable UUID id, @RequestBody ClientDto clientDto) {
+        return clientService.updateClient(id, clientDto);
+    }
+
+    @DeleteMapping("/{id]")
+    public ResponseEntity<String> deleteClient(@PathVariable UUID id) {
+        clientService.deleteClient(id);
+        return ResponseEntity.ok("Client deleted");
+    }
+
 }
